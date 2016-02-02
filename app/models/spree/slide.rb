5085dc826c6109be2ce6c1278155a0be7a67f717
Mohammed Gharbi <simo163@hotmail.co.uk>
2016-02-02 12:56:24 +0100

    changing slides views

--- app/models/spree/slide.rb
+++ app/models/spree/slide.rb
@@ -3,17 +3,16 @@ class Spree::Slide < ActiveRecord::Base
   has_and_belongs_to_many :slide_locations, 
                           class_name: 'Spree::SlideLocation', 
                           join_table: 'spree_slide_slide_locations'
+  belongs_to :product, touch: true
 
   has_attached_file :image,
                     url: '/spree/slides/:id/:style/:basename.:extension',
                     path: ':rails_root/public/spree/slides/:id/:style/:basename.:extension',
                     convert_options: { all: '-strip -auto-orient -colorspace sRGB' }
-  validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
-  
+  validates_attachment :image, presence: true, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+
   scope :published, -> { where(published: true).order('position ASC') }
   scope :location, -> (location){ joins(:slide_locations).where('spree_slide_locations.name = ?', location) }
-
-  belongs_to :product, touch: true
 
   def initialize(attrs = nil)
     attrs ||= {:published => true}
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Spree::Core::Engine.routes.draw do
   namespace :admin do
-    resources :slides do
+    resources :slides, except: :show do
       collection do
         post :update_positions
       end
     end
 
-    resources :slide_locations
+    resources :slide_locations, except: :show
   end
 end
